743316722587613d4bc378c33af131cf546a95d4
Michael Flanagan <michael.flanagan87@gmail.com>
2014-01-30 03:35:35 +0000

    Initial commit - nothing to fancy here

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bueller::Application.config.secret_key_base = 'fb7f69317786b463546b5b9e8c6df41f042ee080932f1418ca0764bae0f66f91089101a3c5c42ee1eb2fc2f672c3f08b058de1aa09e8c485b165786c5c60035e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bueller::Application.config.session_store :cookie_store, key: '_Bueller_session'
